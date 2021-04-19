544ca41cde83c879d58661e3ee4cdb62e6a8d673
Tauqeer Ahmed Khan <>
2021-04-20 00:25:57 +0530

    Day 16

--- Day16/Day16/ContentView.swift
+++ Day16/Day16/ContentView.swift
@@ -7,10 +7,17 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View {    
+    let listOfStudent = ["Harry", "Charlie", "Meghan"]
+    @State private var selectedStudent = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Picker("Select your Student", selection: $selectedStudent) {
+            ForEach(0 ..< listOfStudent.count) { (students) in
+                Text("Student: \(students)")
+            }
+        }
+        Text("You chose:\(listOfStudent[selectedStudent])")
     }
 }
 
